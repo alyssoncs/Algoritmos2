66258a6c1f2a5986210b8f5fd9f08afef1100584
alyssoncs <alysson.cirilo@gmail.com>
2017-05-23 01:47:03 -0300

    functional

--- 3/travel.c
+++ 3/travel.c
@@ -9,15 +9,13 @@ int main(int argc, char const *argv[])
 	char mother[1000];
 	int t = 0;
 
-	//scanf("%d", &t);
-	//getchar();
 	while (1)
 	{
 		t++;
 
-		fgets(father, 101, stdin);
+		fgets(father, 102, stdin);
 		if (father[0] == '#') break;
-		fgets(mother, 101, stdin);
+		fgets(mother, 102, stdin);
 		father[strlen(father)-1] = '\0';
 		mother[strlen(mother)-1] = '\0';
 		int best = travel(father, mother, 0, 0);
@@ -30,26 +28,20 @@ int main(int argc, char const *argv[])
 int travel(char *father, char *mother, int fidx, int midx)
 {
 	int best = 0;
-	int fidx_aux;
 
 	char *mp = strchr(&mother[midx], father[fidx]);
 	if (mp == NULL)
 	{
-		//printf ("entrou nessa bosta\nmother = \"%s\"\nfather[0] = '%c'\n", mother, father[0]);
 		return best;
 	}
 
-	for (fidx_aux = fidx; fidx_aux < strlen(father); fidx_aux++)
+	for ( ; fidx < strlen(father); fidx++)
 	{
-		int aux = travel(father, mother, fidx_aux, mother-mp+1);
+		int aux = travel(father, mother, fidx, mp-mother+1);
 		if (aux > best)
 			best = aux;
 
 	}
-
-	/*int aux = travel(father, mother, fidx_aux, mother-mp+1);
-	if (aux > best)
-		best = aux;*/
 
 	return best+1;
 }
