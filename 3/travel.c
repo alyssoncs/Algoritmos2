0243727c2925c9fab3aeda13949dc289823bfa8a
alyssoncs <alysson.cirilo@gmail.com>
2017-05-22 23:49:32 -0300

    start DP

--- 3/travel.c
+++ 3/travel.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int travel(char *, char *);
+int travel(char *, char *, int, int);
 
 int main(int argc, char const *argv[])
 {
@@ -20,33 +20,34 @@ int main(int argc, char const *argv[])
 		fgets(mother, 101, stdin);
 		father[strlen(father)-1] = '\0';
 		mother[strlen(mother)-1] = '\0';
-		int best = travel(father, mother);
+		int best = travel(father, mother, 0, 0);
 
 		printf("Case #%d: you can visit at most %d cities.\n", t, best);
 	}
 	return 0;
 }
 
-int travel(char *father, char *mother)
+int travel(char *father, char *mother, int fidx, int midx)
 {
 	int best = 0;
-	int fidx;
+	int fidx_aux = fidx;
 
-	char *mp = strchr(mother, father[0]);
+	char *mp = strchr(&mother[midx], father[fidx]);
 	if (mp == NULL)
 	{
-		printf ("entrou nessa bosta\nmother = \"%s\"\nfather[0] = '%c'\n", mother, father[0]);
+		//printf ("entrou nessa bosta\nmother = \"%s\"\nfather[0] = '%c'\n", mother, father[0]);
 		return best;
 	}
 
-	for (fidx = 1; fidx < strlen(father); fidx++) {
-		int aux = travel(&father[fidx], &mother[mp-mother+1]);
+	for (fidx = fidx+1; fidx < strlen(father); fidx++)
+	{
+		int aux = travel(father, mother, fidx, mother-mp+1);
 		if (aux > best)
 			best = aux;
 
 	}
 
-	int aux = travel(father, mp+1);
+	int aux = travel(father, mother, fidx_aux, mother-mp+1);
 	if (aux > best)
 		best = aux;
 
