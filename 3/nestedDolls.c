66577783edd17bd916cd934249336f7b5e5ed9a2
alyssoncs <alysson.cirilo@gmail.com>
2017-05-22 23:33:23 -0300

    travel and nested dolls added

---
+++ 3/nestedDolls.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	int t, m;
+	int dolls[40000];
+
+	scanf("%d", &t);
+	while (t--)
+	{
+		scanf("%d", &m);
+		for (i=0; i<2*m; i++)
+		{
+			scanf("%d", &dolls[i]);
+		}
+
+		
+	}
+	return 0;
+}
---
+++ 3/travel.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int travel(char *, char *);
+
+int main(int argc, char const *argv[])
+{
+	char father[1000];
+	char mother[1000];
+	int t = 0;
+
+	//scanf("%d", &t);
+	//getchar();
+	while (1)
+	{
+		t++;
+
+		fgets(father, 101, stdin);
+		if (father[0] == '#') break;
+		fgets(mother, 101, stdin);
+		father[strlen(father)-1] = '\0';
+		mother[strlen(mother)-1] = '\0';
+		int best = travel(father, mother);
+
+		printf("Case #%d: you can visit at most %d cities.\n", t, best);
+	}
+	return 0;
+}
+
+int travel(char *father, char *mother)
+{
+	int best = 0;
+	int fidx;
+
+	char *mp = strchr(mother, father[0]);
+	if (mp == NULL)
+	{
+		printf ("entrou nessa bosta\nmother = \"%s\"\nfather[0] = '%c'\n", mother, father[0]);
+		return best;
+	}
+
+	for (fidx = 1; fidx < strlen(father); fidx++) {
+		int aux = travel(&father[fidx], &mother[mp-mother+1]);
+		if (aux > best)
+			best = aux;
+
+	}
+
+	int aux = travel(father, mp+1);
+	if (aux > best)
+		best = aux;
+
+	return best+1;
+}
